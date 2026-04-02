a208f7cc5e1dc06d427a4595fa7b0c7a7b512754
Avinash kumar <avinashkumar40977@gmail.com>
2026-04-02 11:03:28 +0530

    [AVINASH] : Updated app.routes.ts file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { AuthComponent } from './components/auth/auth.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HistoryComponent } from './components/history/history.component';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'auth', component: AuthComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
+  { path: 'history', component: HistoryComponent, canActivate: [authGuard] },
   { path: '**', redirectTo: '' }
 ];
